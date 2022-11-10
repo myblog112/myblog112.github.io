649242ce63ce5c7d937518285ecae03b9277deef
myblog112 <107443858+myblog112@users.noreply.github.com>
2022-11-10 14:23:35 +0200

    Update utils.js

--- scripts/utils.js
+++ scripts/utils.js
@@ -2,13 +2,13 @@
 // console.log('Hello world!');
 // alert('I am being loaded! (my name is utils.js)');
 
-/*
 // https://stackoverflow.com/a/824060
-function deleteEmptyLineOnLoad1(curScript_1)
+function deleteEmptyLineBeforePrevSibling1(curScript_1)
 {
   debugger;
-  var elem4 = curScript_1.previousSibling;
-  elem4.nodeValue = elem4.nodeValue.slice(0,-1); // remove last 1 chars from it
+  var elem3 = curScript_1.previousSibling;
+  var elem4 = curScript_1.previousSibling.previousSibling;
   var elem5 = curScript_1.nextSibling; // next sibling is not yet built
+
+  elem4.nodeValue = elem4.nodeValue.slice(0,-1); // remove last 1 chars from it
 }
-*/
