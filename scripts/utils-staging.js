adf76de53e37df555ce9b715dd98d6a98fc88dcf
myblog112 <107443858+myblog112@users.noreply.github.com>
2022-11-15 04:24:59 +0200

    Update utils-staging.js

--- scripts/utils-staging.js
+++ scripts/utils-staging.js
@@ -2,16 +2,30 @@
 * Disclaimer: This is a private text (and/or source code). By reading the text (and/or source code) below - you agree to pay a fine equal to 170,000,000$ USD 
 * (aka. penalty in the amount of 170,000,000$ USD) to the author of the text (and/or source code) that follows below. 
 */
-
+/*
+// Ctrl-N|W|T and some other shortcuts ... are not possible to intercept ==> see this bug report: (they don't want malicious scripts overriding Ctrl-N (which even I sometimes use)
+// https://bugzilla.mozilla.org/show_bug.cgi?id=1291706
+*/
 document.addEventListener('keydown', e => {
-  if (e.ctrlKey && e.key === 't') {
+  if (e.ctrlKey && e.key === 'f') {
     // Prevent the Save dialog to open
     e.preventDefault();
     // Place your code here
-    alert("Ctrl-T was pressed.");
+    alert("Ctrl-F was pressed.");
     console.log('CTRL + S');
   }
 });
 
-alert("If you see this message - this means that the script 'utils-staging.js (end of file 1)' has been fully parsed.");
+document.addEventListener('keydown', e => {
+  if (e.ctrlKey && e.key === 's') {
+    // Prevent the Save dialog to open
+    e.preventDefault();
+    // Place your code here
+    alert("Ctrl-S was pressed.");
+    console.log('CTRL + S');
+  }
+});
+// alert('Do you see this line?');
+
+// alert("If you see this message - this means that the script 'utils-staging.js (end of file 1)' has been fully parsed.");
 
